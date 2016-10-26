b809aaeba9b85dcb66db6d6cb302bbf4cb8dca7f
AlexShmatko <alex.shmatko@active-bridge.com>
2016-10-27 00:04:34 +0300

    Versioning API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post 'signin' => 'sessions#create'
+      get 'singout' => 'sessions#destroy'
+      post 'signup' => 'users#create'
+
+      resources :users
+    end
+  end
 end
